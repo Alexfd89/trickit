f66251c21f6547402fe334c562b6d7cecb0f7773
unknown <Alexfd89>
2019-03-02 20:46:40 +0200

    app header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,19 @@
 import React from "react"
 import withStyles from '@material-ui/core/styles/withStyles'
 import styles from '../styles'
-import Header from "./Header"
-
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
 
 const Layout = ({ children }) => (
   <div>
-    <Header />
+    <div>
+      <AppBar position="static" style={{ background: '#212121' }} >
+        <Toolbar>
+          <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+            TRICKIT
+                        </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
     <div
       style={{
         margin: `0 auto`,
