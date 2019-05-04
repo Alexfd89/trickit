cab049a490e5886b339835cda3bf7fa8310f1a2a
Alexfd89 <alexfd89@gmail.com>
2019-05-04 17:33:00 +0300

    bug fix

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -9,9 +9,9 @@ export class VideoSection extends Component {
                 <div style={{maxWidth: '500px', margin: '0 auto'}}>
                     <iframe
                         width="100%"
-                        height="315"
-                        src={src}
+                        height="315px"
                         title={src}
+                        src={src}
                         frameBorder="0"
                         allowFullScreen
                     />
